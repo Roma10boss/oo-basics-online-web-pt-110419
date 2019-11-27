6dc06a070be9450181e0d74d407a39f3d6c098ea
Romario Gustave <romagustave10@gmail.com>
2019-11-27 14:24:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,4 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :size, :page_count, :genre
-  attr_reader :title
- 
-  def initialize(title)
-    @title = title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
  
 end
