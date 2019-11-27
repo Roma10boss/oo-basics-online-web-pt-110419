ef2758f2ac3eef7911e63a1e580bd09b0c773f47
Romario Gustave <romagustave10@gmail.com>
2019-11-27 14:26:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,13 @@
 # Make your shoe class here!
 class Shoe 
+  attr_accessor :size, :color, :material , :condition
+  attr_reader :brand
  
+  def initialize(title)
+    @title = title
+  end
+ 
+  def condition
+    puts "the shoe has been repaired!"
+  end
 end
